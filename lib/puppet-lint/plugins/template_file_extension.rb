d7e0da329c4105579ea28499dee5e2dd53f9e607
Dean Wilson <dean.wilson@gmail.com>
2020-07-12 11:19:57 +0100

    Fix some Lint issues.
    Still not green but requires more of a rewrite to be rubocop compliant
    without scattering lots of overrides

--- lib/puppet-lint/plugins/template_file_extension.rb
+++ lib/puppet-lint/plugins/template_file_extension.rb
@@ -1,42 +1,40 @@
 PuppetLint.new_check(:template_file_extension) do
   def check
-
     template_extensions = {
-      epp: '.epp',
+      epp:      '.epp',
       template: '.erb',
     }
 
     resource_indexes.each do |resource|
-      if resource[:type].value == 'file'
-        resource[:param_tokens].select { |param_token|
-          param_token.value == 'content'
-        }.each do |content_token|
+      next unless resource[:type].value == 'file'
 
-          value_token = content_token.next_code_token.next_code_token
+      param_tokens = resource[:param_tokens].select { |pt| pt.value == 'content' }
 
-          if matched = value_token.value.match(/^(template|epp)$/)
-            template_function = matched[1]
-            extension = template_extensions[template_function.to_sym]
+      param_tokens.each do |content_token|
+        value_token = content_token.next_code_token.next_code_token
 
-            current_token = value_token.next_token
-            current_token = current_token.next_token while current_token.type == :WHITESPACE
+        if (matched = value_token.value.match(/^(template|epp)$/))
+          template_function = matched[1]
+          extension = template_extensions[template_function.to_sym]
 
-            # iterate over all the code tokens until we hit the closing ')'
-            until current_token.type == :RPAREN || current_token.type == :LBRACE
-              current_token = current_token.next_code_token
+          current_token = value_token.next_token
+          current_token = current_token.next_token while current_token.type == :WHITESPACE
 
-              if current_token.type == :SSTRING && !current_token.value.end_with?(extension)
+          # iterate over all the code tokens until we hit the closing ')'
+          until current_token.type == :RPAREN || current_token.type == :LBRACE
+            current_token = current_token.next_code_token
 
-                warning = "all #{template_function} file names should end with #{extension}"
+            if current_token.type == :SSTRING && !current_token.value.end_with?(extension)
 
-                notify :warning, {
-                  message:     warning,
-                  line:        value_token.line,
-                  column:      value_token.column,
-                  param_token: content_token,
-                  value_token: value_token,
-                }
-              end
+              warning = "all #{template_function} file names should end with #{extension}"
+
+              notify :warning, {
+                message:     warning,
+                line:        value_token.line,
+                column:      value_token.column,
+                param_token: content_token,
+                value_token: value_token,
+              }
             end
           end
         end
